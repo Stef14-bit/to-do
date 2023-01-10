a7fed8b1e6e40f553cd7a5bf0d4556af6f6a154d
stefan <stefan@stefan.com>
2023-01-10 15:28:43 +0100

    add todo done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,44 @@
+import { useState } from "react";
 import "./App.css";
+import { v1 as uuidv1 } from "uuid";
 
 function App() {
-  return <div className="App"></div>;
+  const id = uuidv1();
+  const [todo, setTodo] = useState([
+    {
+      id: id,
+      title: "",
+      completed: false,
+    },
+  ]);
+  const [todos, setTodos] = useState([]);
+
+  const handleTodoChange = (e) => {
+    setTodo((prevState) => ({ ...prevState, id: id, title: e.target.value }));
+  };
+
+  const handleTodoSubmit = () => {
+    setTodos([...todos, todo]);
+    setTodo((prevState) => ({ ...prevState, title: "" }));
+  };
+  return (
+    <div className="App">
+      <div className="todo-container">
+        {" "}
+        <form onSubmit={(e) => e.preventDefault()}>
+          <input value={todo.title} onChange={handleTodoChange} />
+          <button onClick={handleTodoSubmit}>Submit</button>
+        </form>
+        {todos.map((e, i) => (
+          <ul>
+            <li>{e.id.split("-")[0]}</li>
+            <li>{e.title}</li>
+            <li>{e.completed}</li>
+          </ul>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
